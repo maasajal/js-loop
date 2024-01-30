ceb214df03927df44d9c66188525e11216119a4e
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-01-30 14:03:03 +0200

    Commitment print 60 times using for and while loop

---
+++ for/1 commitment.js
@@ -0,0 +1,14 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+let commitment =
+  "I will invest at least 6 hrs every single day for next 60 days!";
+
+for (let i = 1; i <= 60; i++) {
+  console.log(i, commitment);
+}
