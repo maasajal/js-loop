27a9c2e9ecc6f9b6f3f35bffc2dad2688e006c96
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-01-30 14:52:31 +0200

    Countdown using for and while loop

--- for/5 countDown.js
+++ for/5 countDown.js
@@ -1,8 +1,6 @@
 /***
-
 Implement a countdown timer that counts down from 81 to 65.
-
- */
-
-
-/*programming hero*/
+***/
+for (let i = 81; i >= 65; i--) {
+  console.log(i);
+}
--- while/5 countdown.js
+++ while/5 countdown.js
@@ -1,8 +1,8 @@
 /***
-
 Implement a countdown timer that counts down from 21 to 15.
-
- */
-
-
-/*programming hero*/
+***/
+let count = 21;
+while (count >= 15) {
+  console.log(count);
+  count--;
+}
