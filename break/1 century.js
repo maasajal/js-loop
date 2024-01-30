c7c0ff8503c1c76c325419ffa4ddb977dfc86d62
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-01-30 15:20:53 +0200

    used break in loop

--- break/1 century.js
+++ break/1 century.js
@@ -1,5 +1,9 @@
-/* 
-
+/*
 Write a loop 1 to 200. Use break to exit the loop once you find 100.
-
 */
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  if (i === 100) {
+    break;
+  }
+}
--- break/3 square break.js
+++ break/3 square break.js
@@ -1,3 +1,9 @@
-/* 
+/*
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+for (let i = 1; i <= 100; i++) {
+  if (Math.sqrt(i) % 2 === 0) {
+    console.log(i);
+    break;
+  }
+}
