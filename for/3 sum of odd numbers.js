69b4a2233fe1825178565f58fa29493f981028ac
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-01-30 14:37:31 +0200

    Sum of odd and even numbers display using for and while loop

--- for/3 sum of odd numbers.js
+++ for/3 sum of odd numbers.js
@@ -1,18 +1,23 @@
-
-
 /***
-
 Subtask-1:
-
 Display sum of all the odd numbers from 91 to 129.
-
- */
 /***
-
 Subtask-2:
-
 Display sum of all the even numbers from 51 to 85.
-
- */
-
-/*programming hero*/
+*/
+let oddSum = 0;
+let evenSum = 0;
+for (let i = 91; i <= 129; i++) {
+  if (i % 2 !== 0) {
+    oddSum += i;
+    // console.log(i);
+  }
+}
+console.log("Sum of odd Numbers from 91 to 129: ", oddSum);
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 === 0) {
+    evenSum += i;
+    // console.log(i);
+  }
+}
+console.log("Sum of even Numbers from 51 to 85: ", evenSum);
--- while/3 sum of odd numbers.js
+++ while/3 sum of odd numbers.js
@@ -1,18 +1,29 @@
-
-
 /***
 
 Subtask-1:
-
 Display sum of all the odd numbers from 81 to 131.
-
- */
 /***
-
 Subtask-2:
-
 Display sum of all the even numbers from 206 to 311.
-
  */
+let oddSum = 0;
+let evenSum = 0;
+let odd = 81;
+let even = 206;
+while (odd <= 131) {
+  if (odd % 2 !== 0) {
+    oddSum += odd;
+    // console.log(odd);
+  }
+  odd++;
+}
+console.log("Sum of all the odd numbers from 81 to 131 is ", oddSum);
 
-/*programming hero*/
+while (even <= 311) {
+  if (even % 2 === 0) {
+    evenSum += even;
+    // console.log(even);
+  }
+  even++;
+}
+console.log("Sum of all the even numbers from 206 to 311 is ", evenSum);
