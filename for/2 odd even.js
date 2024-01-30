096ce3702560cdac40269ea3289546d1d4d47221
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-01-30 14:18:23 +0200

    Odd and even numbers print using for and while loop

--- for/2 odd even.js
+++ for/2 odd even.js
@@ -1,18 +1,20 @@
 /***
-
 Subtask-1:
-
 Find all the odd numbers from 61 to 100.
-
- */
-
-
 /***
-
 Subtask-2:
-
 Find all the even numbers from 78 to 98.
-
- */
-
-/*programming hero*/
+***/
+console.log("Odd numbers");
+for (let i = 61; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+console.log("==================");
+console.log("Even numbers");
+for (let i = 78; i <= 98; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
--- while/2 odd even.js
+++ while/2 odd even.js
@@ -1,18 +1,25 @@
 /***
-
 Subtask-1:
-
 Find all the odd numbers from 61 to 100.
-
- */
-
-
 /***
-
 Subtask-2:
-
 Find all the even numbers from 78 to 98.
-
  */
-
 /*programming hero*/
+console.log("Odd numbers");
+let odd = 61;
+while (odd <= 100) {
+  if (odd % 2 !== 0) {
+    console.log(odd);
+  }
+  odd++;
+}
+console.log("==================");
+console.log("Even numbers");
+let even = 78;
+while (even <= 98) {
+  if (even % 2 === 0) {
+    console.log(even);
+  }
+  even++;
+}
